7959df41acc1180b00579fc0812da6cceb951916
“clamplay” <1354809038@qq.com>
2025-02-24 13:28:36 +0800

    参数完善

--- main_RC49.py
+++ main_RC49.py
@@ -7,17 +7,18 @@
 from config import cfg
 from flow import process_flow
 
-cfg.version = "v022117"
-cfg.dataset_name = "RC-49_filtered"
-cfg.cont_label_h5_key = "labels"
+# torch.cuda.set_device(0)  # 设置当前 GPU 设备为 0 (如不指定,默认并行)
+
+# cfg.version = "v022117"  # 固定版本
+cfg.dataset_name = "RC-49_filtered"  # 原数据集图片量过大,筛选后另置一个数据集
 cfg.class_label_h5_key = "types"
 cfg.img_size = 64
 # 这个49是固定的,是特定于[0,90]数据集的总类别!
-# 但是要注意,数据集中存储的0~49,其中缺6,这些是类别标签, 而非类别标签索引! 计算交叉熵时候必须用索引!
-# 为了简化实验,仅在数据处理时打印一下对应关系,后续的class_labels存储的都是索引!
+# 但是要注意,数据集中存储的0~49,其中缺6,这些应视作类别标签, 而非类别标签索引! 计算交叉熵时候必须用索引!
+# 为了简化实验,仅在数据处理时打印一下标签与索引对应关系,后续的class_labels存储的都是索引!
 cfg.num_classes = 49
-cfg.min_label = 0.0  # 只看0~90度的数据,用于数据筛选与归一化
-cfg.max_label = 90.0  # 只看0~90度的数据,用于数据筛选与归一化
+cfg.min_label = 0.0  # scoping 0~90度的数据,用于数据筛选与归一化
+cfg.max_label = 90.0  # scoping 0~90度的数据,用于数据筛选与归一化
 cfg.min_img_num_per_label = 5
 cfg.max_img_num_per_label = 9999
 cfg.threshold_type = "soft"
@@ -26,12 +27,12 @@ cfg.kappa = -2.0
 
 # 评估相关
 cfg.comp_fid = True
-cfg.dump_fake_for_niqe = True  # 是否导出用于 NIQE 计算的图像
+cfg.dump_fake_for_niqe = False  # 是否导出用于 NIQE 计算的图像
 cfg.comp_is_and_fid_only = False  # 是否只计算 IS 和 FID（减少计算量）
+cfg.n_fake_per_label = 200  # 每个连续标签(整数)生成多少张图像用于评估
 cfg.pretrained_ae_pth = "ckpt_AE_epoch_200_seed_2020_CVMode_False.pth"
 cfg.pretrained_cnn4cont_pth = "ckpt_PreCNNForEvalGANs_ResNet34_regre_epoch_200_seed_2020_CVMode_False.pth"
 cfg.pretrained_cnn4class_pth = "ckpt_PreCNNForEvalGANs_ResNet34_class_epoch_200_seed_2020_classify_49_chair_types_CVMode_False.pth"
-cfg.n_fake_per_label = 10  # 每个连续标签(整数)生成多少张图像用于评估
 
 # 算法流程
 process_flow()
--- main_UTKFace.py
+++ main_UTKFace.py
@@ -7,19 +7,19 @@
 from config import cfg
 from flow import process_flow
 
-# torch.cuda.set_device(0)  # 设置当前 GPU 设备为 0
+# torch.cuda.set_device(0)  # 设置当前 GPU 设备为 0 (如不指定,默认并行)
 
-cfg.version = "v022117"
+# cfg.version = "v022117"  # 固定版本
 cfg.dataset_name = "UTKFace"
-cfg.cont_label_h5_key = "labels"
 cfg.class_label_h5_key = "races"
 cfg.img_size = 64
 cfg.num_classes = 5
-cfg.min_label = 1  # 只看1~60岁,用于数据筛选与归一化
-cfg.max_label = 60  # 只看1~60岁,用于数据筛选与归一化
+cfg.min_label = 1  # scoping 1~60岁,用于数据筛选与归一化
+cfg.max_label = 60  # scoping 1~60岁,用于数据筛选与归一化
 cfg.min_img_num_per_label = 60
 cfg.max_img_num_per_label = 9999
 cfg.threshold_type = "soft"
+cfg.loss_type = "vanilla"
 
 # 训练epoch或iter调成1,用于测试
 cfg.n_iters = 40000
@@ -30,10 +30,10 @@ cfg.epoch_net_y2h = 500
 cfg.comp_fid = True
 cfg.dump_fake_for_niqe = False  # 是否导出用于 NIQE 计算的图像
 cfg.comp_is_and_fid_only = False  # 是否只计算 IS 和 FID（减少计算量）
+cfg.n_fake_per_label = 1000  # 每个连续标签(整数)生成多少张图像用于评估
 cfg.pretrained_ae_pth = "ckpt_AE_epoch_200_seed_2020_CVMode_False.pth"
 cfg.pretrained_cnn4cont_pth = "ckpt_PreCNNForEvalGANs_ResNet34_regre_epoch_200_seed_2020_CVMode_False.pth"
 cfg.pretrained_cnn4class_pth = "ckpt_PreCNNForEvalGANs_ResNet34_class_epoch_200_seed_2020_classify_5_races_CVMode_False.pth"
-cfg.n_fake_per_label = 1000  # 每个连续标签(整数)生成多少张图像用于评估
 
 # 算法主体流程
 process_flow()
